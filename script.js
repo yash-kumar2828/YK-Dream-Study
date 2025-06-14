e38cf5c929ffea01d4df3e74813fbc45288c2ef7
Yash Kumar <yk5708428@gmail.com>
2025-06-14 17:26:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('toggleButton').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+});
+
+
+
